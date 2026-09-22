0ec779afe498fd2ef0443dc79b6364b35a258149
DonBarz <paulaurelius@web.de>
2026-09-23 01:50:09 +0200

    added command support to edit the config ingame. it very much isnt working but ill just push anyways so :lemon: can help me

--- src/main/java/de/donbarz/tickinator/BlockConfig.java
+++ src/main/java/de/donbarz/tickinator/BlockConfig.java
@@ -4,11 +4,14 @@ import dev.lemonnik.fern_config.CConfig;
 import dev.lemonnik.fern_config.CExporter;
 import dev.lemonnik.fern_config.types.*;
 import dev.lemonnik.fern_config.utils.CCategory;
+import dev.lemonnik.fern_config.utils.CValue;
 import dev.lemonnik.fern_config.utils.MaskType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BlockConfig extends CConfig {
 
@@ -26,31 +29,39 @@ public class BlockConfig extends CConfig {
 
     //public final CEnum<TestEnum> test_enum = register(COMPLEX_THINGS, new CEnum<>("thats_enum", "Only values listed above this comment are acceptable", TestEnum.class, TestEnum.TASTY_ENUM));
 
-    public final CMask include_mask = register(COMPLEX_THINGS, new CMask(
-                    "include_list",
-                    "Add blocks here and their entities shall be suppressed",
-                    BuiltInRegistries.BLOCK,
-                    new CEnum<MaskType>(
-                            "mask_type",
-                            "",
-                            MaskType.class,
-                            MaskType.WHITELIST
-                    ),
-                    "*:*"
-            )
-    );
+    public final CMask include_mask = updateIncludeMask(new String[0]);
 
-    public final CMask exclude_mask = register(COMPLEX_THINGS, new CMask(
-                    "exclude_list",
-                    "Exclude certain blocks. If you add a block to both lists, this will have priority",
-                    BuiltInRegistries.BLOCK,
-                    new CEnum<MaskType>(
-                            "mask_type",
-                            "",
-                            MaskType.class,
-                            MaskType.BLACKLIST
-                    ),
-                    "*:*"
-            )
-    );
+    public final CMask exclude_mask = updateExcludeMask(new String[0]);
+
+    public CMask updateIncludeMask (String[] newMaskStr) {
+        return register(COMPLEX_THINGS, new CMask(
+                        "include_list",
+                        "Add blocks here and their entities shall be suppressed",
+                        BuiltInRegistries.BLOCK,
+                        new CEnum<MaskType>(
+                                "mask_type",
+                                "",
+                                MaskType.class,
+                                MaskType.WHITELIST
+                        ),
+                        newMaskStr
+                )
+        );
+    }
+
+    public CMask updateExcludeMask (String[] newMaskStr) {
+        return register(COMPLEX_THINGS, new CMask(
+                        "exclude_list",
+                        "Add blocks to exclude them from suppression. If added to both, this has priority",
+                        BuiltInRegistries.BLOCK,
+                        new CEnum<MaskType>(
+                                "mask_type",
+                                "",
+                                MaskType.class,
+                                MaskType.BLACKLIST
+                        ),
+                        newMaskStr
+                )
+        );
+    }
 }
--- src/main/java/de/donbarz/tickinator/Tickinator.java
+++ src/main/java/de/donbarz/tickinator/Tickinator.java
@@ -1,10 +1,21 @@
 package de.donbarz.tickinator;
 
+import dev.lemonnik.fern_config.utils.Mask;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.blocks.BlockStateArgument;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 
 public class Tickinator implements ModInitializer {
     public static final String MOD_ID = "tickinator";
@@ -14,15 +25,60 @@ public class Tickinator implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        BLOCK_CONFIG.reload();
+        LOGGER.info(String.valueOf(BLOCK_CONFIG.reload()));
 
         // adding command to reload the config at runtime
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(Commands.literal("tickinator")
                     .then(Commands.literal("reload").executes(context -> {
                         BLOCK_CONFIG.reload();
+                        context.getSource().getPlayerOrException().sendSystemMessage(Component.literal("Reloaded block entity suppression!"));
                         return 0;
-                    })));
+                    }))
+                    .then(Commands.literal("suppress").then(Commands.argument("block", BlockStateArgument.block(registryAccess))
+                            .then(Commands.literal("add").executes(context -> {
+                                Block block = BlockStateArgument.getBlock(context, "block").getState().getBlock();
+                                context.getSource().getPlayerOrException().sendSystemMessage(Component.literal("Now suppressing ").append(block.getName()));
+
+
+                                // add a block to the existing lists
+                                ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(block);
+
+                                if (BLOCK_CONFIG.exclude_mask.get().matches(blockId)) {
+                                    String[] newMaskString = BLOCK_CONFIG.exclude_mask.get().getEntries().stream().map(ResourceLocation::toString).filter(string -> !string.equals(blockId.toString())).toArray(String[]::new);
+                                    BLOCK_CONFIG.updateExcludeMask(newMaskString);
+                                }
+                                if (!BLOCK_CONFIG.include_mask.get().matches(blockId)) {
+                                    ArrayList<ResourceLocation> newEntries = new ArrayList<>(BLOCK_CONFIG.include_mask.get().getEntries().stream().toList());
+                                    newEntries.add(blockId);
+                                    String[] newMaskString = newEntries.stream().map(ResourceLocation::toString).toArray(String[]::new);
+                                    BLOCK_CONFIG.updateIncludeMask(newMaskString);
+                                }
+
+                                BLOCK_CONFIG.save();
+                                LOGGER.info(String.valueOf(BLOCK_CONFIG.reload()));
+
+                                return 0;
+                            }))
+                            .then(Commands.literal("remove").executes(context -> {
+                                Block block = BlockStateArgument.getBlock(context, "block").getState().getBlock();
+                                context.getSource().getPlayerOrException().sendSystemMessage(Component.literal("No longer suppressing ").append(block.getName()));
+
+                                // remove a block from the existing lists
+                                ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(block);
+
+                                if (BLOCK_CONFIG.include_mask.get().matches(blockId)) {
+                                    String[] newMaskString = BLOCK_CONFIG.include_mask.get().getEntries().stream().map(ResourceLocation::toString).filter(string -> !string.equals(blockId.toString())).toArray(String[]::new);
+                                    BLOCK_CONFIG.updateIncludeMask(newMaskString);
+                                }
+
+                                BLOCK_CONFIG.save();
+                                LOGGER.info(String.valueOf(BLOCK_CONFIG.reload()));
+
+                                return 0;
+                            }))
+                    ))
+            );
         });
     }
 }
